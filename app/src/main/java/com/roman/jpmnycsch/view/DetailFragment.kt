69dcd8852d9e9a2d947db66d2953ed04aca7e1f4
roman <roman.edjlali@gmail.com>
2023-09-06 11:43:59 -0700

    A bit cleanup the code before start working on Hilt.

--- app/src/main/java/com/roman/jpmnycsch/view/DetailFragment.kt
+++ app/src/main/java/com/roman/jpmnycsch/view/DetailFragment.kt
@@ -6,11 +6,14 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.activity.OnBackPressedCallback
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
+import androidx.navigation.fragment.findNavController
 import com.roman.jpmnycsch.R
 import com.roman.jpmnycsch.databinding.FragmentDetailBinding
+import com.roman.jpmnycsch.databinding.FragmentListBinding
 import com.roman.jpmnycsch.model.SATScore
 import com.roman.jpmnycsch.viewmodel.DetailViewModel
 
@@ -19,55 +22,57 @@ import com.roman.jpmnycsch.viewmodel.DetailViewModel
  * Owner: Roman Edjlali
  * Date Created: 09/01/2023 8:28 AM
  */
-class DetailFragment : Fragment() {
+class DetailFragment : Fragment(R.layout.fragment_detail) {
 
     private val tag = javaClass.simpleName
 
-    private lateinit var detailBinding: FragmentDetailBinding
+    private var detailBinding: FragmentDetailBinding? = null
     private var schUuid = 0
     private var dbnIndex = "dbn"
 
     lateinit var  viewModel: DetailViewModel
     private var currentSatScore: SATScore? = null
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-
-        detailBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_detail, container, false)
-        return detailBinding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        //val btnList = view.findViewById<Button>(R.id.btnList)
+        detailBinding = FragmentDetailBinding.bind(view)
         arguments?.let {
             //schUuid = DetailFragmentArgs.fromBundle(it).schUuid
             dbnIndex = DetailFragmentArgs.fromBundle(it).dbnIndex
         }
+        val callBack= object: OnBackPressedCallback(true){
+            override fun handleOnBackPressed() {
+                findNavController().popBackStack()
+            }
+        }
+        requireActivity().onBackPressedDispatcher.addCallback(callBack)
 
         Log.d(tag, "***>>> schUuid is $schUuid & dbnIndex: $dbnIndex")
         viewModel = ViewModelProviders.of(this).get(DetailViewModel::class.java)
         //viewModel.fetch(schUuid)
         viewModel.fetchByDbn(dbnIndex)
         observeViewModel()
+
+
     }
 
     private fun observeViewModel() {
         viewModel.satScoreLiveData.observe(viewLifecycleOwner, Observer {
             currentSatScore = it
             it?.let {
-                detailBinding.tvDetailDbn.text = it.dbn
-                detailBinding.tvDetailSchoolName.text = it.school_name
-                detailBinding.tvMathScore.text = it.sat_math_avg_score.toString()
-                detailBinding.tvReadingScore.text = it.sat_critical_reading_avg_score.toString()
-                detailBinding.tvTestTakers.text = it.num_of_sat_test_takers.toString()
-                detailBinding.tvWritingScore.text = it.sat_writing_avg_score.toString()
+                detailBinding?.tvDetailDbn?.text = it.dbn
+                detailBinding?.tvDetailSchoolName?.text = it.school_name
+                detailBinding?.tvMathScore?.text = it.sat_math_avg_score.toString()
+                detailBinding?.tvReadingScore?.text = it.sat_critical_reading_avg_score.toString()
+                detailBinding?.tvTestTakers?.text = it.num_of_sat_test_takers.toString()
+                detailBinding?.tvWritingScore?.text = it.sat_writing_avg_score.toString()
             }
         })
+    }
+
+    override fun onDestroy() {
+        detailBinding = null
+        super.onDestroy()
     }
 
 }
--- app/src/main/java/com/roman/jpmnycsch/view/ListFragment.kt
+++ app/src/main/java/com/roman/jpmnycsch/view/ListFragment.kt
@@ -19,58 +19,22 @@ import com.roman.jpmnycsch.viewmodel.ListViewModel
  * Owner: Roman Edjlali
  * Date Created: 09/01/2023 8:23 AM
  */
-class ListFragment : Fragment() {
+class ListFragment : Fragment(R.layout.fragment_list) {
 
     private val tag = javaClass.simpleName
-    private lateinit var listBinding: FragmentListBinding
+    private var listBinding: FragmentListBinding? = null
 
     lateinit var viewModel: ListViewModel
     private val schoolAdapter = SchoolAdapter(arrayListOf())
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        /*arguments?.let {
-
-        }
-        viewModel = ViewModelProviders.of(this).get(SchoolViewModel::class.java)
-        viewModel.refresh()
-
-        listBinding.schoolList.apply {
-            layoutManager = LinearLayoutManager(context)
-            adapter = schoolAdapter
-        }
-
-        observeViewModel()*/
-    }
-
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        listBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_list, container, false
-        )
-        return listBinding.root
-    }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        /*val btnDetail = view.findViewById<Button>(R.id.btnDetail)
-        btnDetail.setOnClickListener{
-            val action = ListFragmentDirections.actionDetailFragment()
-            action.schUuid = 5
-            Navigation.findNavController(it).navigate(action)
-        }*/
-
-        arguments?.let {
-
-        }
+        listBinding = FragmentListBinding.bind(view)
         viewModel = ViewModelProviders.of(this).get(ListViewModel::class.java)
         viewModel.refresh()
 
-        listBinding.schoolList.apply {
+        listBinding?.schoolList?.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = schoolAdapter
         }
@@ -81,25 +45,30 @@ class ListFragment : Fragment() {
     fun observeViewModel() {
         viewModel.schools.observe(viewLifecycleOwner, Observer {schools ->
             schools?.let {
-                listBinding.schoolList.visibility = View.VISIBLE
+                listBinding?.schoolList?.visibility = View.VISIBLE
                 schoolAdapter.updateSchoolList(it) }
         })
 
         viewModel.schoolLoadError.observe(viewLifecycleOwner, Observer {
             it?.let {
-                listBinding.listError.visibility = if (it) View.VISIBLE else View.GONE
+                listBinding?.listError?.visibility = if (it) View.VISIBLE else View.GONE
             }
         })
 
         viewModel.loading.observe(viewLifecycleOwner, Observer { isLoading ->
             isLoading?.let {
-                listBinding.loadingView.visibility = if(it) View.VISIBLE else View.GONE
+                listBinding?.loadingView?.visibility = if(it) View.VISIBLE else View.GONE
                 if(it) {
-                    listBinding.listError.visibility = View.GONE
-                    listBinding.schoolList.visibility = View.GONE
+                    listBinding?.listError?.visibility = View.GONE
+                    listBinding?.schoolList?.visibility = View.GONE
                 }
             }
         })
+    }
+
+    override fun onDestroy() {
+        listBinding = null
+        super.onDestroy()
     }
 
 }
